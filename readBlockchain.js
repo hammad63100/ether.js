a6634a30e8f70cd90a15f78f7e2653fa42323a71
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2024-07-11 15:49:10 +0500

    Update readBlockchain.js

--- readBlockchain.js
+++ readBlockchain.js
@@ -1,1 +1,2 @@
-const {ethers} = require("ethers")
+const {ethers} = require("ethers");
+const provider = new ether.providers.JsonRpcProvider(`https://mainnet.infura.io.infura.io/v3/82843bd181984a0da8832ac77678ffcb`)
