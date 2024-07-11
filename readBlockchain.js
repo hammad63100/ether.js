4bdbedc0b2e118bae272554f059276bcaa08f61b
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2024-07-11 15:52:40 +0500

    first

--- readBlockchain.js
+++ readBlockchain.js
@@ -1,2 +1,6 @@
-const {ethers} = require("ethers");
-const provider = new ether.providers.JsonRpcProvider(`https://mainnet.infura.io.infura.io/v3/82843bd181984a0da8832ac77678ffcb`)
+const { ethers } = require("ethers");
+const provider = new ether.providers.JsonRpcProvider(`https://mainnet.infura.io.infura.io/v3/82843bd181984a0da8832ac77678ffcb`);
+
+const querryBlockchain = async () => {
+    const block = await provider.getBlockNumber
+}
