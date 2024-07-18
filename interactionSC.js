8c3e68fa9ef49d66198e178c612eab02b27a967c
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2024-07-18 10:49:45 +0500

    Create interactionSC.js

---
+++ interactionSC.js
@@ -0,0 +1,7 @@
+
+const {ethers, providers} = require("ethers");
+
+const provider = new ethers.providers.JsonRpcProvider(`https://holesky.infura.io/v3/28f029bc75b84ed9a1cce9c27495011b`);
+
+
+const 
